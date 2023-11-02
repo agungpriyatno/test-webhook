437f37281289facd05eaa2fbb16f0edd670ffd4d
Agung Priyatno <agungpriyatno666@gmail.com>
2023-11-02 12:47:41 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -8,10 +8,14 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.get('/:slug', (req, res) => {
-    const slug = req.params.slug;
-    console.log("Triggred on : " + slug);
-    res.send(`Triggred ${slug}`);
+app.post("/", (req, res) => {
+    console.log(req.body);
+    /*
+      we need to respond back with 200 to let telegram know that we
+      have received the update. Failing to do so will result in telegram
+      not sending further updates after the first one.
+    */
+    res.status(200).send('ok');
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,16 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/:slug', (req: Request, res: Response) => {
-  const slug = req.params.slug
-  console.log("Triggred on : " +slug);
-  res.send(`Triggred ${slug}`);
-});
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  
+  /* 
+    we need to respond back with 200 to let telegram know that we 
+    have received the update. Failing to do so will result in telegram 
+    not sending further updates after the first one.
+  */
+  res.status(200).send('ok');
+})
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
